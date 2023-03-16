60db0a7dca3509b757cbf978b061d0e752dff13b
Ashton Stockenberg <as.j.stockenberg@baypath.net>
2023-03-16 09:02:31 -0400

    really bad client and server test ahahaha B)

--- Client.java
+++ Client.java
@@ -1,5 +1,53 @@
-public class Client {
-    public static void main(String[] args) {
-        
+import java.net.*;
+import java.io.*;
+
+public class Client extends Thread{
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) {
+        try{
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            while((inputLine = in.readLine()) != null){
+                System.out.println(inputLine);
+            }
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public String sendMessage(String msg) {
+        try{
+            out.println(msg);
+            String resp = in.readLine();
+            return resp;
+        }
+        catch(IOException e){
+            System.out.println(e);
+            return "Error Occured";
+        }
+    }
+
+    public void stopConnection() {
+        try{
+            in.close();
+            out.close();
+            if(clientSocket != null) clientSocket.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args){
+        Client client = new Client();
+        while (true){
+            client.startConnection("127.0.0.1", 6666);
+        }
     }
 }
--- Server.java
+++ Server.java
@@ -1,5 +1,42 @@
-public class Server {
+import java.net.*;
+import java.io.*;
+
+public class Server{
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startServer(int port) {
+        try{
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader stdIn =new BufferedReader(new InputStreamReader(System.in));
+            
+            while(true){
+                out.println("Server: " + stdIn.readLine());
+            }
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
+
+    public void stopServer() {
+        try{
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+    }
     public static void main(String[] args) {
-        
+        Server server=new Server();
+        server.startServer(6666);
     }
 }

87a09ac2339b729fd75b9649fda3d694a9a544c3
Ashton Stockenberg <as.j.stockenberg@baypath.net>
2023-03-30 13:01:55 -0400

    auto reconnect to server added

--- Client.py
+++ Client.py
@@ -1,8 +1,4 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("10.45.76.113", 6666))
-
-currentCommand = ""
 
 def forward():
     print("Forward")
@@ -19,17 +15,30 @@ def right():
 def stop():
     print("Stopping")
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+currentCommand = ""
+
+s.connect(("10.45.76.113", 6666))
+
 while True:
-    command = s.recv(1024).decode().strip()
+    try:
+        command = s.recv(1024).decode().strip()
 
-    if(command == "forward" and currentCommand != "forward"): forward()
-    if(command == "backward" and currentCommand != "backward"): backward()
-    if(command == "left" and currentCommand != "left"): left()
-    if(command == "right"  and currentCommand != "right"): right()
+        if(command == "forward" and currentCommand != "forward"): forward()
+        if(command == "backward" and currentCommand != "backward"): backward()
+        if(command == "left" and currentCommand != "left"): left()
+        if(command == "right"  and currentCommand != "right"): right()
 
-    if(command == "unforward" and currentCommand == "forward"): stop()
-    if(command == "unbackward" and currentCommand == "backward"): stop()
-    if(command == "unleft" and currentCommand == "left"): stop()
-    if(command == "unright" and currentCommand == "right"): stop()
+        if(command == "unforward" and currentCommand == "forward"): stop()
+        if(command == "unbackward" and currentCommand == "backward"): stop()
+        if(command == "unleft" and currentCommand == "left"): stop()
+        if(command == "unright" and currentCommand == "right"): stop()
 
-    currentCommand = command;   
+        currentCommand = command;   
+    except:
+       try:
+           s.close()
+           s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+           s.connect(("10.45.76.113", 6666))
+       except IOError:
+           print(IOError)

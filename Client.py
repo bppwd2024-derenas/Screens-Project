9d45ed5f200fa3ab1cff29aebc38870b27a2ecdb
Ashton Stockenberg <as.j.stockenberg@baypath.net>
2023-03-31 09:18:40 -0400

    fixed some stuff and added speed

--- Client.py
+++ Client.py
@@ -1,26 +1,54 @@
 import socket
 
+leftSpeed = 0
+rightSpeed = 0
+
 def forward():
     print("Forward")
+    global leftSpeed
+    global rightSpeed
+
+    leftSpeed = 100
+    rightSpeed = 100
 
 def backward():
     print("Backward")
+    global leftSpeed
+    global rightSpeed
+
+    leftSpeed = -100
+    rightSpeed = -100
 
 def left():
     print("Left")
+    global leftSpeed
+    global rightSpeed
+
+    leftSpeed = 100
+    rightSpeed = 50
 
 def right():
     print("Right")
+    global leftSpeed
+    global rightSpeed
+
+    leftSpeed = 50
+    rightSpeed = 100
 
 def stop():
     print("Stopping")
+    global leftSpeed
+    global rightSpeed
+
+    leftSpeed = 0
+    rightSpeed = 0
 
 currentCommand = ""
 
 while True:
     try:
         command = s.recv(1024).decode().strip()
-
+        
         if(command == "forward" and currentCommand != "forward"): forward()
         elif(command == "backward" and currentCommand != "backward"): backward()
         elif(command == "left" and currentCommand != "left"): left()
@@ -31,10 +59,10 @@ while True:
         elif(command == "unleft" and currentCommand == "left"): stop()
         elif(command == "unright" and currentCommand == "right"): stop()
 
-        currentCommand = command;   
+        currentCommand = command
+        print(leftSpeed, rightSpeed)   
     except:
        try:
-           s.close()
            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
            s.connect(("10.45.76.113", 6666))
        except:
